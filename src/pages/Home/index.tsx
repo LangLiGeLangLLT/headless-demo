625260ea0527ead625c5909456b5669efca1e390
LLT <418797644@qq.com>
2022-10-25 00:00:00 +0800

    feat: dropdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,21 @@
+import { lazy } from 'ice';
+
+const Dropdown = lazy(() => import('@/components/Dropdown'));
+const DropdownExample = lazy(() => import('@/components/DropdownExample'));
+
 const Home = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div
+      className="
+        h-screen w-screen
+        bg-gradient-to-r from-violet-500 to-indigo-500
+        flex justify-center items-center
+      "
+    >
+      {/* <Dropdown /> */}
+      <DropdownExample />
+    </div>
+  );
 };
 
 export default Home;
