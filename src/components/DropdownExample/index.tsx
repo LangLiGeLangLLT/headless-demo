d7f6a0d240c18b7618e5d3536b30f0e2ad5c512c
LLT <418797644@qq.com>
2022-10-27 00:02:01 +0800

    feat: add dropdown

--- src/components/DropdownExample/index.tsx
+++ src/components/DropdownExample/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { Menu, Transition } from '@headlessui/react';
 import { Fragment } from 'react';
 import { ChevronDownIcon } from '@heroicons/react/20/solid';
@@ -6,14 +7,7 @@ export default function Example() {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-        <Menu.Button
-          className="
-            inline-flex w-full justify-center rounded-md bg-black
-            bg-opacity-20 px-4 py-2 text-sm font-medium text-white
-            hover:bg-opacity-30 focus:outline-none focus-visible:ring-2
-            focus-visible:ring-white focus-visible:ring-opacity-75
-          "
-        >
+        <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
           Options
           <ChevronDownIcon className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100" aria-hidden="true" />
         </Menu.Button>
@@ -27,12 +21,7 @@ export default function Example() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items
-          className="
-            absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100
-            rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none
-          "
-        >
+        <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="px-1 py-1 ">
             <Menu.Item>
               {({ active }) => (
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,15 +5,9 @@ const DropdownExample = lazy(() => import('@/components/DropdownExample'));
 
 const Home = () => {
   return (
-    <div
-      className="
-        h-screen w-screen
-        bg-gradient-to-r from-violet-500 to-indigo-500
-        flex justify-center items-center
-      "
-    >
-      {/* <Dropdown /> */}
-      <DropdownExample />
+    <div className="h-screen w-screen bg-gradient-to-r from-violet-500 to-indigo-500 flex justify-center items-center">
+      <Dropdown />
+      {/* <DropdownExample /> */}
     </div>
   );
 };
